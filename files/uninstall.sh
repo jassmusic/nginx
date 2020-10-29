d83c7fdcd4bdacedd0281d55033b0b76d132d9f0
jassmusic <59103051+jassmusic@users.noreply.github.com>
2020-10-29 12:18:46 +0900

    Update uninstall.sh

--- files/uninstall.sh
+++ files/uninstall.sh
@@ -1,4 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
+sv down nginx
+sv down php-fpm
+apt -y remove nginx php-fpm
 rm -rf ~/app/pre_start.sh
 rm -rf ~/app/data/nginx/www/index.html
 rm -rf ~/app/data/nginx/www/phpinfo.php
