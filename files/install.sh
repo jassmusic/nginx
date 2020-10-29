fd06a4ccc677512976bafe832f9ce3286cd0cceb
jassmusic <59103051+jassmusic@users.noreply.github.com>
2020-10-29 12:08:11 +0900

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -1,19 +1,30 @@
-#!/bin/sh
-mkdir -p /app/data/nginx
-mkdir -p /app/data/nginx/www
-apk update
-apk upgrade
-apk add nginx
-mkdir -p /run/nginx
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
-cp /app/data/custom/nginx/files/nginx.conf /app/data/nginx/nginx.conf
-ln -s /app/data/nginx/nginx.conf /etc/nginx/nginx.conf
-apk add php7-fpm php7-mcrypt php7-soap php7-openssl php7-gmp php7-pdo_odbc php7-json php7-dom php7-pdo php7-zip php7-mysqli php7-sqlite3 php7-apcu php7-pdo_pgsql php7-bcmath php7-gd php7-odbc php7-pdo_mysql php7-pdo_sqlite php7-gettext php7-xmlreader php7-xmlrpc php7-bz2 php7-iconv php7-pdo_dblib php7-curl php7-ctype php7-simplexml php7-mbstring php7-fileinfo php7-session
-mv /etc/php7/php.ini /etc/php7/php.ini.orig
-cp /app/data/custom/nginx/files/php.ini /app/data/nginx/php.ini
-ln -s /app/data/nginx/php.ini /etc/php7/php.ini
-cp /app/data/custom/nginx/files/pre_start.sh /app/pre_start.sh
-cp /app/data/custom/nginx/files/index.html /app/data/nginx/www/index.html
-cp /app/data/custom/nginx/files/phpinfo.php /app/data/nginx/www/phpinfo.php
-cp /app/data/custom/nginx/files/guide.php /app/data/nginx/www/guide.php
-chmod 777 -R /app/data/nginx
+#!/data/data/com.termux/files/usr/bin/bash
+# nginx install
+
+# php-fpm update
+sed -i 's/listen\ =\ \/data\/data\/com.termux\/files\/usr\/var\/run\/php-fpm.sock/listen\ =\ 127.0.0.1:9000/' /data/data/com.termux/files/usr/etc/php-fpm.d/www.conf
+
+# nginx conf update
+cd /data/data/com.termux/files/usr/etc/nginx
+curl -LO https://raw.githubusercontent.com/jassmusic/termux/master/nginx.conf
+
+# make nginx dir
+mkdir -p ~/app/data/nginx
+mkdir -p ~/app/data/nginx/www
+cd ~/app/data/nginx
+ln -s /data/data/com.termux/files/usr/etc/nginx/nginx.conf nginx.conf
+cp ~/app/data/custom/nginx/files/php.ini ~/app/data/nginx
+cp ~/app/data/custom/nginx/files/index.html ~/app/data/nginx/www
+cp ~/app/data/custom/nginx/files/phpinfo.php ~/app/data/nginx/www
+cp ~/app/data/custom/nginx/files/guide.php ~/app/data/nginx/www
+
+# make pre_start
+cd ~/app
+cat <<EOF >pre_start.sh
+#!/data/data/com.termux/files/usr/bin/bash
+sv up nginx
+sv up php-fpm
+EOF
+chmod +x pre_start.sh
+
+chmod 777 -R ~/app/data/nginx
