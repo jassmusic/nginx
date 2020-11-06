7992635411fcd729e88334dc177537bb277892cd
jassmusic <59103051+jassmusic@users.noreply.github.com>
2020-11-07 04:21:41 +0900

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -5,7 +5,7 @@ pkg install termux-services
 apt -y install nmap sqlite nginx php-fpm
 
 # php-fpm update
-sed -i 's/listen\ =\ \/data\/data\/com.termux\/files\/usr\/var\/run\/php-fpm.sock/listen\ =\ 127.0.0.1:9000/' /data/data/com.termux/files/usr/etc/php-fpm.d/www.conf
+#sed -i 's/listen\ =\ \/data\/data\/com.termux\/files\/usr\/var\/run\/php-fpm.sock/listen\ =\ 127.0.0.1:9000/' /data/data/com.termux/files/usr/etc/php-fpm.d/www.conf
 
 # nginx conf update
 #cd /data/data/com.termux/files/usr/etc/nginx
